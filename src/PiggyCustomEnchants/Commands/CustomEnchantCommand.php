a3f59de5ea70d172069595b01bc049daf3399709
Mr. Piggy <mcpepig123@gmail.com>
2017-05-01 22:35:52 -0700

    Added /ce list & now enchant with /ce enchant like PC version

--- src/PiggyCustomEnchants/Commands/CustomEnchantCommand.php
+++ src/PiggyCustomEnchants/Commands/CustomEnchantCommand.php
@@ -2,6 +2,7 @@
 
 namespace PiggyCustomEnchants\Commands;
 
+use PiggyCustomEnchants\CustomEnchants\CustomEnchants;
 use PiggyCustomEnchants\Main;
 use pocketmine\command\CommandSender;
 use pocketmine\command\ConsoleCommandSender;
@@ -23,7 +24,7 @@ class CustomEnchantCommand extends PluginCommand
     {
         parent::__construct($name, $plugin);
         $this->setDescription("Enchant with custom enchants");
-        $this->setUsage("/customenchant <enchant> <level> [player]");
+        $this->setUsage("/customenchant <enchant|list>");
         $this->setAliases(["ce"]);
         $this->setPermission("piggycustomenchants.command.ce");
     }
@@ -39,22 +40,36 @@ class CustomEnchantCommand extends PluginCommand
         if (!$this->testPermission($sender)) {
             return true;
         }
-        if (count($args) < 2) {
-            $sender->sendMessage("/customenchant <enchant> <level> [player]");
+        if (count($args) < 1) {
+            $sender->sendMessage("/customenchant <enchant|list>");
             return false;
         }
-        $target = $sender;
-        if (isset($args[2])) {
-            $target = $this->getPlugin()->getServer()->getPlayer($args[2]);
+        switch($args[0]){
+            case "list":
+                $enchants = array();
+                foreach (CustomEnchants::$enchantments as $id => $enchant){
+                   array_push($enchants, $enchant->getName());
+                }
+                $sender->sendMessage(implode(", ", $enchants));
+                break;
+            case "enchant":
+                if (count($args) < 3) {
+                    $sender->sendMessage("/customenchant enchant <enchant> <level> [player]");
+                }
+                $target = $sender;
+                if (isset($args[3])) {
+                    $target = $this->getPlugin()->getServer()->getPlayer($args[3]);
+                }
+                if (!$target instanceof Player) {
+                    if($target instanceof ConsoleCommandSender){
+                        $sender->sendMessage("§cPlease provide a player.");
+                        return false;
+                    }
+                    $sender->sendMessage("§cInvalid player.");
+                    return false;
+                }
+                $this->getPlugin()->addEnchantment($target->getInventory()->getItemInHand(), $args[1], $args[2], $target);
+                break;
         }
-        if (!$target instanceof Player) {
-            if($target instanceof ConsoleCommandSender){
-                $sender->sendMessage("§cPlease provide a player.");
-                return false;
-            }
-            $sender->sendMessage("§cInvalid player.");
-            return false;
-        }
-        $this->getPlugin()->addEnchantment($target->getInventory()->getItemInHand(), $args[0], $args[1], $target);
     }
 }
