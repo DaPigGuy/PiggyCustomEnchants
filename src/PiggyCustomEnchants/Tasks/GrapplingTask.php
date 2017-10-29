133d0933f781d11ab5e2b892ea8e74ad1a486164
Mr. Piggy <mcpepig123@gmail.com>
2017-10-29 16:17:04 -0700

    Fixes #28

--- src/PiggyCustomEnchants/Tasks/GrapplingTask.php
+++ src/PiggyCustomEnchants/Tasks/GrapplingTask.php
@@ -41,11 +41,13 @@ class GrapplingTask extends PluginTask
         $g = -0.08;
         $d = $location->distance($entityloc);
         $t = $d;
-        $v_x = (1.0 + 0.07 * $t) * ($location->x - $entityloc->x) / $t;
-        $v_y = (1.0 + 0.03 * $t) * ($location->y - $entityloc->y) / $t - 0.5 * $g * $t;
-        $v_z = (1.0 + 0.07 * $t) * ($location->z - $entityloc->z) / $t;
-        $v = $this->entity->getMotion();
-        $v->setComponents($v_x, $v_y, $v_z);
-        $this->entity->setMotion($v);
+        if ($t > 0) { //$t is zero if collides with the shooter of the grappling arrow, we dont want the shooter to grapple themselves
+            $v_x = (1.0 + 0.07 * $t) * ($location->x - $entityloc->x) / $t;
+            $v_y = (1.0 + 0.03 * $t) * ($location->y - $entityloc->y) / $t - 0.5 * $g * $t;
+            $v_z = (1.0 + 0.07 * $t) * ($location->z - $entityloc->z) / $t;
+            $v = $this->entity->getMotion();
+            $v->setComponents($v_x, $v_y, $v_z);
+            $this->entity->setMotion($v);
+        }
     }
 }
