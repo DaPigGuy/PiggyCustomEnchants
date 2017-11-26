0410bab413aaf62216dc1d496755112e34b5de45
Mr. Piggy <mcpepig123@gmail.com>
2017-11-25 23:37:46 -0800

    How has this gone unnoticed?

--- src/PiggyCustomEnchants/Tasks/CactusTask.php
+++ src/PiggyCustomEnchants/Tasks/CactusTask.php
@@ -32,7 +32,7 @@ class CactusTask extends PluginTask
     public function onRun(int $currentTick)
     {
         foreach ($this->plugin->getServer()->getOnlinePlayers() as $player) {
-            foreach ($player->getInventory()->getContents() as $item) {
+            foreach ($player->getInventory()->getArmorContents() as $item) {
                 if ($this->plugin->getEnchantment($item, CustomEnchants::CACTUS) !== null) {
                     foreach ($player->getLevel()->getNearbyEntities($player->getBoundingBox()->grow(1, 0, 1), $player) as $p) {
                         $ev = new EntityDamageByEntityEvent($player, $p, EntityDamageEvent::CAUSE_CONTACT, 1);
