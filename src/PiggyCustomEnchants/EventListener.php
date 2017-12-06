ddcd6d7a2856bdffb0c0933baeaafc0be07c9c57
Mr. Piggy <mcpepig123@gmail.com>
2017-12-05 16:57:25 -0800

    Fix another error with Hallucination when hitting mobs

--- src/PiggyCustomEnchants/EventListener.php
+++ src/PiggyCustomEnchants/EventListener.php
@@ -465,7 +465,7 @@ class EventListener implements Listener
             if ($enchantment !== null) {
                 $chance = 5 * $enchantment->getLevel();
                 $random = mt_rand(0, 100);
-                if ($random <= $chance && isset($this->plugin->hallucination[$entity->getLowerCaseName()]) !== true && $entity instanceof Player) {
+                if ($random <= $chance && $entity instanceof Player && isset($this->plugin->hallucination[$entity->getLowerCaseName()]) !== true) {
                     $this->plugin->hallucination[$entity->getLowerCaseName()] = true;
                     $task = new HallucinationTask($this->plugin, $entity, $entity->getPosition());
                     $handler = $this->plugin->getServer()->getScheduler()->scheduleRepeatingTask($task, 1);
