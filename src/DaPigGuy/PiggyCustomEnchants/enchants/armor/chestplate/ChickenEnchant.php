6eeb9cd761efadfc9bdfe2e120f4f93d7882b9c8
DaPigGuy <mcpepig123@gmail.com>
2019-10-19 16:40:43 -0700

    Fixed potential crash w/ Chicken

--- src/DaPigGuy/PiggyCustomEnchants/enchants/armor/chestplate/ChickenEnchant.php
+++ src/DaPigGuy/PiggyCustomEnchants/enchants/armor/chestplate/ChickenEnchant.php
@@ -37,7 +37,7 @@ class ChickenEnchant extends TickingEnchantment
             }
             $drop = array_rand($drops);
             $drop = explode(":", $drops[$drop]);
-            $item = count($drop) < 3 ? Item::get(Item::GOLD_INGOT, 0, 1) : Item::get($drop[0], $drop[1], $drop[2]);
+            $item = count($drop) < 3 ? Item::get(Item::GOLD_INGOT, 0, 1) : Item::get((int)$drop[0], (int)$drop[1], (int)$drop[2]);
             $vowels = ["a", "e", "i", "o", "u"];
             $player->getLevel()->dropItem($player, $item, $player->getDirectionVector()->multiply(-0.4));
             $player->sendTip(TextFormat::GREEN . "You have laid a" . (in_array(strtolower($item->getName()[0]), $vowels) ? "n " : " ") . $item->getName() . "...");
