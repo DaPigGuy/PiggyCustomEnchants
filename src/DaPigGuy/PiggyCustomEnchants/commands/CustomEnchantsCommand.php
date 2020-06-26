e4467a3ff9e92a9e8f6acf0da95df745afaa495a
HighTec <32067693+HighTec90@users.noreply.github.com>
2020-06-26 20:47:12 +0200

    Update CustomEnchantsCommand.php

--- src/DaPigGuy/PiggyCustomEnchants/commands/CustomEnchantsCommand.php
+++ src/DaPigGuy/PiggyCustomEnchants/commands/CustomEnchantsCommand.php
@@ -44,11 +44,11 @@ class CustomEnchantsCommand extends BaseCommand
 
     public function prepare(): void
     {
-        $this->registerSubCommand(new AboutSubCommand($this->plugin, "about", "Displays basic information about the plugin"));
-        $this->registerSubCommand(new EnchantSubCommand($this->plugin, "enchant", "Apply an enchantment on an item"));
-        $this->registerSubCommand(new InfoSubCommand($this->plugin, "info", "Get info on a custom enchant"));
-        $this->registerSubCommand(new ListSubCommand($this->plugin, "list", "Lists all registered custom enchants"));
-        $this->registerSubCommand(new NBTSubCommand($this->plugin, "nbt", "Displays NBT tags of currently held item"));
-        $this->registerSubCommand(new RemoveSubCommand($this->plugin, "remove", "Remove an enchantment from an item"));
+        $this->registerSubCommand(new AboutSubCommand("about", "Displays basic information about the plugin"));
+        $this->registerSubCommand(new EnchantSubCommand("enchant", "Apply an enchantment on an item"));
+        $this->registerSubCommand(new InfoSubCommand("info", "Get info on a custom enchant"));
+        $this->registerSubCommand(new ListSubCommand("list", "Lists all registered custom enchants"));
+        $this->registerSubCommand(new NBTSubCommand("nbt", "Displays NBT tags of currently held item"));
+        $this->registerSubCommand(new RemoveSubCommand("remove", "Remove an enchantment from an item"));
     }
 }
