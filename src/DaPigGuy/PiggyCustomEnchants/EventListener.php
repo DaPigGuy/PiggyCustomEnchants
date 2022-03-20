aecc590d69ffba77986dc504ab8b6384f659b91e
DaPigGuy <mcpepig123@gmail.com>
2022-03-20 03:00:01 -0700

    Cleanup pt. II

--- src/DaPigGuy/PiggyCustomEnchants/EventListener.php
+++ src/DaPigGuy/PiggyCustomEnchants/EventListener.php
@@ -312,8 +312,8 @@ class EventListener implements Listener
                             $newLevel = $existingEnchant->getLevel() === $newLevel ? $newLevel + 1 : $newLevel;
                         }
                         if (
-                            (($enchantment instanceof CustomEnchant && (
-                                    !Utils::itemMatchesItemType($itemClicked, $enchantment->getItemType()) || !Utils::checkEnchantIncompatibilities($itemClicked, $enchantment)))
+                            ($enchantment instanceof CustomEnchant &&
+                                (!Utils::itemMatchesItemType($itemClicked, $enchantment->getItemType()) || !Utils::checkEnchantIncompatibilities($itemClicked, $enchantment))
                             ) ||
                             $itemClicked->getCount() !== 1 ||
                             $newLevel > $enchantment->getType()->getMaxLevel() ||
